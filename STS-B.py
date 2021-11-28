69df9446043859566006b52fa40eb92ae8844069
Shiwen Ni <56249874+nishiwen1214@users.noreply.github.com>
2021-11-28 16:59:36 +0800

    Update STS-B.py

--- STS-B.py
+++ STS-B.py
@@ -1,7 +1,7 @@
 #! -*- coding:utf-8 -*-
 # https://github.com/nishiwen1214/GLUE-bert4keras
 # 句子对回归任务，STS-B数据集
-# pearson_corr:89.31 spearman_corr:88.90
+# pearson_corr:89.20 spearman_corr:88.77
 
 import numpy as np
 from bert4keras.backend import keras, set_gelu, K
@@ -13,6 +13,11 @@ from bert4keras.snippets import open
 from keras.layers import Dropout, Dense
 from scipy.stats import pearsonr, spearmanr
 import numpy as np
+import os
+from tqdm import tqdm
+import csv
+# 选择使用第几张GPU卡，'0'为第一张
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 set_gelu('tanh')  # 切换gelu版本
 
@@ -36,6 +41,21 @@ def load_data(filename):
             else:
                 _,_,_,_,_,_,_, text1, text2, label = l.strip().split('\t')
                 D.append((text1, text2, float(label)))
+    return D
+
+def load_data_test(filename):
+    """加载数据
+    单条格式：(文本1, 文本2, 标签y)
+    """
+    D = []
+    i = 1
+    with open(filename, encoding='utf-8') as f:
+        for l in f:
+            if i == 1: # 跳过数据第一行
+                i = 2
+            else:
+                _,_,_,_,_,_,_, text1, text2 = l.strip().split('\t')
+                D.append((text1, text2, 0))
     return D
 
 # 加载数据集
@@ -126,7 +146,27 @@ class Evaluator(keras.callbacks.Callback):
             (pearson_corr,spearman_corr, self.best_val_pearson_corr)
         )
 
+def test_predict(in_file, out_file):
+    """输出测试结果到文件
+    结果文件可以提交到 https://gluebenchmark.com 评测。
+    """
+    test_data = load_data_test(in_file)
+    test_generator = data_generator(test_data, batch_size)
 
+    results = []
+    for x_true, _ in tqdm(test_generator, ncols=0):
+        y_pred = model.predict(x_true)
+        results.extend(y_pred)
+        
+    with open(out_file,'w',encoding='utf-8') as f:
+        csv_writer = csv.writer(f, delimiter='\t')
+        csv_writer.writerow(["index","prediction"])
+        # 写入tsv文件内容
+        for i, pred in enumerate(results):
+            csv_writer.writerow([i,min(pred[0], 5.0)])  # 保证值不超过5.0
+        # 关闭文件
+    f.close()
+    
 if __name__ == '__main__':
 
     evaluator = Evaluator()
@@ -137,7 +177,13 @@ if __name__ == '__main__':
         epochs=10,
         callbacks=[evaluator]
     )
-
+    
+    model.load_weights('best_model_STS-B.weights')
+#   预测测试集，输出到结果文件
+    test_predict(
+        in_file = './datasets/STS-B/test.tsv',
+        out_file = './results/STS-B.tsv'
+    )
 else:
 
     model.load_weights('best_model_STS-B.weights')
